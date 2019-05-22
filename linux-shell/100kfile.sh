ea69ef20a07bf925ec23b611ed4b6b4eaeb6976f
Phuc. Tran Trong (4) <phuctt4@vng.com.vn>
2019-05-22 09:03:59 +0700

    update

--- linux-shell/100kfile.sh
+++ linux-shell/100kfile.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 read -p  "Enter pattern: " pattern
-read -p "Enter option: " opt
 files=$(sudo find ~ -name $pattern -type f -size +100k)
-if [ "$opt" = "compress" ]; then
-        sudo zip 'test.zip' $files >> 'test-compress.log'
-elif [ "$opt" = "delete" ]; then
-        sudo rm -v $files > 'test-delete.log'
-else
-        echo "Not found option: $opt"
-fi
+
+for file in $files
+do
+    echo $file
+done
+
+for file in $files
+do 
+    echo -e "\e[34m\e[1m$file\e[0m\e[39m"
+    read -p "delete or compress (d or c, press any key to skip)? " opt
+    if [ "$opt" = "c" ]; then
+        sudo zip 'test.zip' $file >> 'test-compress.log'
+    elif [ "$opt" = "d" ]; then
+        sudo rm -v $file >> 'test-delete.log'
+    else
+        echo -e "\e[31m\e[1mSkip\e[0m\e[39m ${file}"
+    fi
+done
